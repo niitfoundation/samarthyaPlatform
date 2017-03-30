74b8b8d207de915baee415b3e6e2d4b201b71e84
prakulkumar <dhiman404prakul@gamil.com>
2017-03-30 22:01:49 +0530

    changes in index files

--- server/api/v1/skills/index.js
+++ server/api/v1/skills/index.js
@@ -1,16 +1,14 @@
 /*
  * What this resource or module will do?
- * Handle CRUD operations for 'Reference data' used for Knowledge model
+ * Handle Create and Find operation for 'skills node in neo4j' used for Knowledge model by administrator
  *
  * i.e., operations this module will support are typically like
  *
- * - Get all data (HTTP GET /reference)
- * - Update nodes and relations
- * - Delete nodes and relations
- * - Add nodes and relations
+ * - Get all skills in graph database
+ * - Add a skills node in database
  *
- * Recommended to mount routes of this module as '/reference'
- * i.e., if a client wants to make a request it will start from '/reference/<route defined in the module>'
+ * Recommended to mount routes of this module as '/skills'
+ * i.e., if a client wants to make a request it will start from '/skills/<route defined in the module>'
  *
  */
 module.exports = require('./skill.router');
