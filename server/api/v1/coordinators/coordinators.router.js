227d47c3759b231debd862e09bd011dfa27017c7
pankushmanchanda <manchandapankush@gmail.com>
2017-03-31 12:06:04 +0530

    Refactoring in Code

--- server/api/v1/coordinators/coordinators.router.js
+++ server/api/v1/coordinators/coordinators.router.js
@@ -12,7 +12,6 @@ const fs = require('fs');
  * Actual URI will be HTTP POST /users/
  */
 // router.post('/', function(req, res) {
-//     console.log("commmm")
 //     let coordinateData = req.body;
 //     logger.debug('Get object and store into coordinateData');
 //     try {
@@ -37,84 +36,83 @@ const fs = require('fs');
 // });
 
 router.post('/upload', function(req, res) {
-    try{
-    var form = new formidable.IncomingForm();
+    try {
+        var form = new formidable.IncomingForm();
 
-form.parse(req, function(err, fields, files) {
-    fs.readFile(files['uploads[]'].path,{encoding: 'utf-8'},function(err,data){
-        if(err)
-        console.log(err)
-        else{
-            
-             UserCtrl.addBulkData(JSON.parse(data)).then((successResult) => {
-            logger.info('Saved successfully and return back');
-            
-            var options = {
-   host: 'localhost',
-   port: 3001,
-   path: '/save?datas='+successResult.data._id,
-   method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-};
+        form.parse(req, function(err, fields, files) {
+            fs.readFile(files['uploads[]'].path, { encoding: 'utf-8' }, function(err, data) {
+                if (err)
+                    console.log(err)
+                else {
 
-var requestHttp = http.request(options,function(response) {
-   response.setEncoding('utf8');
-   response.on('data', function (chunk) {
-       console.log("on-data")
-   });
-    response.on('end', function () {
-            console.log("on-end")
-      response.send('ok')
-   });
-});
+                    UserCtrl.addBulkData(JSON.parse(data)).then((successResult) => {
+                        logger.info('Saved successfully and return back');
 
-requestHttp.end();
+                        var options = {
+                            host: 'localhost',
+                            port: 3001,
+                            path: '/save?datas=' + successResult.data._id,
+                            method: 'POST',
+                            headers: {
+                                'Content-Type': 'application/json',
+                            },
+                        };
 
-        })
+                        var requestHttp = http.request(options, function(response) {
+                            response.setEncoding('utf8');
+                            response.on('data', function(chunk) {
+                                console.log("on-data")
+                            });
+                            response.on('end', function() {
+                                console.log("on-end")
+                                response.send('ok')
+                            });
+                        });
+
+                        requestHttp.end();
+
+                    })
+                }
+            });
+        });
+        form.on('file', function(name, file) {
+            console.log('Uploaded ' + file.name);
+
+        });
+        return res.status(201).send({ "msg": "Importing is in progress..Will import that soon" });
+
+    } catch (err) {
+        return res.send({ "msg": "Error in importing please try later..." });
+
     }
-});
-});
-    form.on('file', function (name, file){
-        console.log('Uploaded ' + file.name);
-
-    });
-    return res.status(201).send({"msg":"Importing is in progress..Will import that soon"});
-
-}
-catch(err){
-    return res.send({"msg":"Error in importing please try later..."});
-
-}
 
 
-// // });
-//             return res.status(201).send(successResult);
-//         }, (errResult) => {
-//             // Log the error for internal use
-//             logger.error('Internal error occurred');
-//             return res.status(500).send({ error: 'Internal error occurred, please try later..!' });
-//         });
-//     } catch (err) {
-//         // Log the Error for internal use
-//         logger.fatal('Exception occurred' + err);
-//         res.send({ error: 'Failed to complete successfully, please check the request and try again..!' });
-//         return;
-//     }
+    // // });
+    //             return res.status(201).send(successResult);
+    //         }, (errResult) => {
+    //             // Log the error for internal use
+    //             logger.error('Internal error occurred');
+    //             return res.status(500).send({ error: 'Internal error occurred, please try later..!' });
+    //         });
+    //     } catch (err) {
+    //         // Log the Error for internal use
+    //         logger.fatal('Exception occurred' + err);
+    //         res.send({ error: 'Failed to complete successfully, please check the request and try again..!' });
+    //         return;
+    //     }
 });
 
 
-router.get('/import-history',function(req,res){
-   
+router.get('/import-history', function(req, res) {
+
 
     try {
         coordinateCtrl.getImportHistory().then((successResult) => {
-            logger.info('Get successResult successfully and return back');
-            return res.status(201).send(successResult);
-        },
+                logger.info('Get successResult successfully and return back');
+                return res.status(201).send(successResult);
+            },
             (err) => {
-                logger.error('Internal error occurred'+err);
+                logger.error('Internal error occurred' + err);
                 return res.status(500).send({ error: 'Internal error occurred, please try later..!' });
             });
     } catch (err) {
@@ -129,18 +127,18 @@ router.get('/import-history',function(req,res){
 
 
 
-router.get('/failure-history',function(req,res){
-   let documentId=req.query.documentId;
-   console.log(documentId)
+router.get('/failure-history', function(req, res) {
+    let documentId = req.query.documentId;
+    console.log(documentId)
 
     try {
         coordinateCtrl.getFailureImportHistory(documentId).then((successResult) => {
-            logger.info('Get successResult successfully and return back');
-            console.log(successResult)
-            return res.status(201).send(successResult);
-        },
+                logger.info('Get successResult successfully and return back');
+                console.log(successResult)
+                return res.status(201).send(successResult);
+            },
             (err) => {
-                logger.error('Internal error occurred'+err);
+                logger.error('Internal error occurred' + err);
                 return res.status(500).send({ error: 'Internal error occurred, please try later..!' });
             });
     } catch (err) {
