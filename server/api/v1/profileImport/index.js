fac2b96f2907ab20f76e28c3e6c9dabb86c7084f
pankushmanchanda <manchandapankush@gmail.com>
2017-04-05 03:36:32 +0530

    issue fixed

---
+++ server/api/v1/profileImport/index.js
@@ -0,0 +1,11 @@
+/*
+ * What this resource or module will do?
+ * Handles Add operations related to profileImport and get the profileImport histories
+ *
+ * i.e., operations this module will support are typically like
+ *
+ * - Import bulk profiles of users
+ * - Get importhistory of which admin imported
+ */
+
+module.exports = require('./profileImport.router');
