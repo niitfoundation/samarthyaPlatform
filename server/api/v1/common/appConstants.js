ca7fab20b443fd7248820ef830eee0b48b340ffb
GowthamJeeva <gowthamjeeva55@gmail.com>
2017-05-09 09:55:36 +0530

    removed unnecessary spaces in email content

--- server/api/v1/common/appConstants.js
+++ server/api/v1/common/appConstants.js
@@ -21,9 +21,9 @@ const predefinedStopwords = ['knows', 'know', 'person', 'persons', 'skill', 'ski
 const emailTemplate = {
 
     header: `<div style='padding: 20px 0;text-align: center;background-color:#f2d9e6;color:#80bfff;margin:auto'><h1>SAMARTHYA</h1></div><br>`,
-    passwordReset: `<span style='font-size:14px;color:#92b9b9;padding-left:10%'>Hi,you can reset your password by clicking the below button.
+    passwordReset: `<span style='font-size:14px;color:#92b9b9;'>Hi,you can reset your password by clicking the below button.
     Don't share your password to anyone. Happy hiring..!</span><br><br>`,
-    thankingRegistration: `<span style='font-size:14px;color:#92b9b9;padding-left:10%'>Thanks for signing up for Samarthya! 
+    thankingRegistration: `<span style='font-size:14px;color:#92b9b9;'>Thanks for signing up for Samarthya! 
     We're excited to have you as a Samarthya member</span><br><br>`,
     actionButton: `<button type='button' style='background-color : #6666ff;padding: 10px 20px;text-align:center;margin-left:35%;color:white;'>
     <a style=\'text-decoration : none;color : white\' href="`,
