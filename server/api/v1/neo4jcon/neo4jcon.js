202165646a0bb5324dab5bd0a23b38eed8be873f
pankushmanchanda <manchandapankush@gmail.com>
2017-06-13 17:59:53 +0530

    neo4 singleton class

--- server/api/v1/neo4jcon/neo4jcon.js
+++ server/api/v1/neo4jcon/neo4jcon.js
@@ -2,28 +2,16 @@ const neo4j = require('neo4j-driver').v1;
 const logger = require('./../../../../applogger');
 const neo4jconfig = require('./../../../../config/config');
 
-const singleton = (function() {
-    function createInstance() {
-        const neo4jConn = neo4j.driver('bolt://localhost', neo4j.auth.basic(neo4jconfig.NEO4J.USERNAME, neo4jconfig.NEO4J.PASSWORD)).session();
-        return neo4jConn;
-    }
-    return {
+/* single instance class for neo4j connection */
 
-        getInstance: function () {
-            let instance;
-            if (!instance) {
-                instance = createInstance();
-            }
-            return instance;
+function singleton() {
+    let instance = undefined;
+    this.connection = function() {
+        if (!this.instance) {
+            this.instance = neo4j.driver('bolt://172.23.238.140', neo4j.auth.basic(neo4jconfig.NEO4J.USERNAME, neo4jconfig.NEO4J.PASSWORD)).session();
         }
+        return this.instance;
     };
-}());
+}
 
-const connection = function () {
-    const singleInstance = singleton.getInstance();
-    return singleInstance;
-};
-
-module.exports = {
-    connection: connection
-};
+module.exports = exports = new singleton;
